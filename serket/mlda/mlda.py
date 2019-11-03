9bfe7d003ffd8377b712529599b1d4c4f56f7bce
eqs <murashige.satoshi.mi1@is.naist.jp>
2019-11-03 13:38:30 +0900

    clean mlda

--- serket/mlda/mlda.py
+++ serket/mlda/mlda.py
@@ -5,6 +5,11 @@ import random
 import pickle
 import os
 from numba import jit
+import numpy as np
+
+import ..serket as srk
+
+__all__ = ['MLDA']
 
 # ハイパーパラメータ
 __alpha = 1.0
@@ -221,3 +226,50 @@ def train( data, K, num_itr=100, save_dir="model", bias_dz=None, categories=None
     params = save_model( save_dir, n_dz, n_mzw, n_mz, M, dims, categories, mode )
     
     return params
+
+
+class MLDA(srk.Module):
+    def __init__( self, K, weights=None, itr=100, name="mlda", category=None, mode="learn" ):
+        super(MLDA, self).__init__(name, True)
+        self.__K = K
+        self.__weights = weights
+        self.__itr = itr
+        self.__category = category
+        self.__mode = mode
+        self.__n = 0
+        
+        if mode != "learn" and mode != "recog":
+            raise ValueError("choose mode from \"learn\" or \"recog\"")
+        
+    def update(self):
+        data = self.get_observations()
+        Pdz = self.get_backward_msg() # P(z|d)
+
+        M = len( data )     # モダリティ数
+        N = len( data[0] )  # データ数
+        
+        # backward messageがまだ計算されていないときは一様分布にする
+        if Pdz is None:
+            Pdz = np.ones( (N, self.__K) ) / self.__K
+    
+        # データの正規化処理
+        for m in range(M):     
+            data[m][ data[m]<0 ] = 0
+            
+        if self.__weights!=None:
+            for m in range(M):
+                data[m] = (data[m].T / data[m].sum(1)).T * self.__weights[m]
+        
+        for m in range(M):
+            data[m] = np.array( data[m], dtype=np.int32 )
+        
+        save_dir = os.path.join( self.get_name(), "%03d" % self.__n )
+        
+        # MLDA学習
+        Pdz, Pdw = train( data, self.__K, self.__itr, save_dir, Pdz, self.__category, self.__mode )
+        
+        self.__n += 1
+        
+        # メッセージの送信
+        self.set_forward_msg( Pdz )
+        self.send_backward_msgs( Pdw )
