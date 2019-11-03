7357b3fcea1e8f703b6b5279a6eef5837ea82726
eqs <murashige.satoshi.mi1@is.naist.jp>
2019-11-03 13:49:12 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+import os
+from setuptools import setup, find_packages
+
+
+def read(filename):
+    filename = os.path.join(os.path.dirname(__file__), filename)
+    text_type = type(u"")
+    with io.open(filename, mode="r", encoding='utf-8') as fd:
+        return re.sub(text_type(r':[a-z]+:`~?(.*?)`'), text_type(r'``\1``'), fd.read())
+
+
+setup(
+    name="serket",
+    version="0.0.1",
+    url="https://github.com/naka-lab/Serket",
+
+    # author="",
+    # author_email="",
+
+    description="Symbol Emergence in Robotics tool KIT",
+    long_description=read("README.md"),
+
+    packages=find_packages(exclude=('tests',)),
+
+    install_requires=[],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
